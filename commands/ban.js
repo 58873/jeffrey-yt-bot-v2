a5f7d6920c4bf54074abb1d4d34055b26786615b
jeffreyJKG <49994449+jeffreyJKG@users.noreply.github.com>
2021-01-08 11:56:48 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (bot, message, args) => {
 
     // const args = message.content.slice(prefix.length).split(/ +/);
 
