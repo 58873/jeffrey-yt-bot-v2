831781e67a8b1eaf912c2f21585ef16e122b2d6b
jeffreyJKG <49994449+jeffreyJKG@users.noreply.github.com>
2021-01-08 13:00:43 +0100

    Create lockdown.js

---
+++ commands/lockdown.js
@@ -0,0 +1,24 @@
+module.exports.run = async (bot, message, args) => {
+
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry jij kan dit niet");
+
+    await message.channel.overwritePremissons([
+
+        {
+            id : message.guild.roles.cache.find(r => r.name == "@everyone").id,
+            deny: ['SEND_MESSAGES']
+        }
+
+
+
+    ]);
+
+    message.channel.send("**Kanaal is in lockdown!**");
+
+    
+}
+
+
+module.exports.help = {
+    name: "lock",
+}
